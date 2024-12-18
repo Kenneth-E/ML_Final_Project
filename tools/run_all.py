8dbe4f1d6c32b1b8928b1a07ca1afc668669aa27
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2024-12-18 15:38:56 -0500

    Update run_all.py

--- tools/run_all.py
+++ tools/run_all.py
@@ -2,6 +2,10 @@ import create_random_keyboard_mash_texts as mash
 import create_adjacency_matrix as matrix
 import create_overall_feature_file as features
 
-mash.main()
-matrix.main()
-features.main()
+def main():
+    mash.main()
+    matrix.main()
+    features.main()
+
+if __name__ == '__main__':
+    main()
