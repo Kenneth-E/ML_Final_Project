514bdc7aa4950cdadfa6a3c0b4d1aa68d3c6739b
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2024-12-18 05:12:44 -0500

    add dvorak

--- tools/create_random_keyboard_mash_texts.py
+++ tools/create_random_keyboard_mash_texts.py
@@ -1,23 +1,30 @@
 import numpy as np
 
+def qwerty_to_dvorak(string):
+    qwerty = r"""qwertyuiop[]\asdfghjkl;'zxcvbnm,./QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>?"""
+    dvorak = r"""',.pyfgcrl/=\aoeuidhtns-;qjkxbmwvz"<>PYFGCRL?+|AOEUIDHTNS_:QJKXBMWVZ""" # yes, I installed the Dvorak keyboard just to type that
+    translation = str.maketrans(qwerty, dvorak)
+    return string.translate(translation)
+
 def append_string_to_file(filename, string):
-    with open(filename, "a") as file:
+    with open(filename, "a", encoding="utf-8") as file:
         file.write(string)
 
-def create_random_texts(num_files, input_words_filenames, output_sentences_filenames, num_samples, max_sentences, random_seed):
+def create_random_texts(num_files, input_words_filenames, output_sentences_filenames, num_samples, max_sentences, random_seed, convert_to_dvorak_list):
     for file_idx in range(num_files):
         input_filename = input_words_filenames[file_idx]
         output_filename = output_sentences_filenames[file_idx]
+        convert_to_dvorak = convert_to_dvorak_list[file_idx]
 
         # clear output file
-        with open(output_filename, "w") as output_file:
+        with open(output_filename, "w", encoding="utf-8") as output_file:
             pass
 
-        print(input_filename)
+        print(f"{input_filename} -> {output_filename}")
 
         words = list()
 
-        with open(input_filename, "r") as words_file:
+        with open(input_filename, "r", encoding="utf-8") as words_file:
             for line in words_file:
                 # line = words_file.readline()
                 line = line.strip()
@@ -40,7 +47,11 @@ def create_random_texts(num_files, input_words_filenames, output_sentences_filen
                 num_words = np.max([1, int(raw_num_words)])
                 for _ in range(num_words):
                     random_word_idx = rng.integers(low=0, high=len(words), endpoint=False)
-                    sentence += words[random_word_idx]
+                    random_word = words[random_word_idx]
+                    if convert_to_dvorak:
+                        random_word = qwerty_to_dvorak(random_word)
+                    random_word = random_word.lower() # lowercase to make it less easily distinguishable
+                    sentence += random_word
                     sentence += " "
                 sentence += "\n"
                 append_string_to_file(output_filename, sentence)
@@ -50,13 +61,25 @@ def main():
 
     NUM_SAMPLES = 10000
     MAX_SENTENCES = 1
-    NUM_FILES = 2
-    INPUT_WORDS_FILENAMES = [r"..\data\raw\words\keyboard_mash_words.txt", r"..\data\raw\words\random_words.txt"]
-    OUTPUT_SENTENCES_FILENAMES = [r"..\data\utterances\keyboard_mash_sentences.txt",
-                                  r"..\data\utterances\random_sentences.txt"]
+    NUM_FILES = 3
+    INPUT_WORDS_FILENAMES = [
+        r"..\data\raw\words\keyboard_mash_words.txt",
+        r"..\data\raw\words\keyboard_mash_words.txt",
+        r"..\data\raw\words\random_words.txt",
+    ]
+    OUTPUT_SENTENCES_FILENAMES = [
+        r"..\data\utterances\keyboard_mash_sentences.txt",
+        r"..\data\utterances\keyboard_mash_sentences_dvorak_simulated.txt",
+        r"..\data\utterances\random_sentences.txt",
+    ]
+    CONVERT_TO_DVORAK_LIST = [
+        False,
+        True,
+        False,
+    ]
     RANDOM_SEED = 42
 
-    create_random_texts(NUM_FILES, INPUT_WORDS_FILENAMES, OUTPUT_SENTENCES_FILENAMES, NUM_SAMPLES, MAX_SENTENCES, RANDOM_SEED)
+    create_random_texts(NUM_FILES, INPUT_WORDS_FILENAMES, OUTPUT_SENTENCES_FILENAMES, NUM_SAMPLES, MAX_SENTENCES, RANDOM_SEED, CONVERT_TO_DVORAK_LIST)
 
     print("ending create_random_keyboard_mash_texts.py")
 
