e5b3d9cd9797d87e724695e909bf55f16bca0ad0
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2024-12-18 21:27:00 -0500

    remove adaboost-only

--- src/ml_model.py
+++ src/ml_model.py
@@ -271,7 +271,6 @@ def main():
     adaboost = theme_and_variation(adaboost_theme, variation_list)
 
     id3_or_adaboost = id3 + adaboost
-    id3_or_adaboost = adaboost # TODO: remove
 
     TEST_IDS = list()
     TRAIN_IDS = list()
